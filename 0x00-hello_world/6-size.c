d8c4b58c4f997e1d3e48eee373857f66b98c473d
Ronald Siele <kimutaironald98@gmail.com>
2023-02-16 08:50:28 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of char:1 bytes\n");
+printf("size of intr:4 bytes\n");
+printf("size of long int:4 bytes\n");
+printf("size of long long int:8 bytes\n");
+printf("size of float:4 bytes\n");
+}
