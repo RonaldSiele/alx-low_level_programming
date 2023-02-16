8289fc856a108142485ad0eeddd78e535e5f5f76
Ronald Siele <kimutaironald98@gmail.com>
2023-02-16 09:31:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
 const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t num_bytes = write(STDERR_FILENO, message, 46);
-return num_bytes == 46 ? 1 : 0;
+return (num_bytes) == 46 ? 1 : 0;
 }
