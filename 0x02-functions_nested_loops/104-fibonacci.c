73626f418529e2c6f9499ebeb640f03f296994de
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 09:56:27 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int current = 1;
+    int previous = 1;
+    int temp;
+    int count = 2;
+
+    printf("%d, %d, ", current, previous);
+
+    while (count < 98) {
+        temp = current;
+        current += previous;
+        previous = temp;
+        printf("%d, ", current);
+        count++;
+    }
+
+    printf("%d\n", current + previous);
+
+    return 0;
+}
+
