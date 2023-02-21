7aed026f0ccd31887eb589860f76264cd5d89efe
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 10:53:22 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int current = 2;
-    int previous = 1;
-    int temp;
-    int sum = 2;
-
-    while (current <= 4000000) {
-        temp = current;
-        current += previous;
-        previous = temp;
-        if (current % 2 == 0) {
-            sum += current;
-        }
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+int current = 2;
+int previous = 1;
+int temp;
+int sum = 2;
+while (current <= 4000000) {
+temp = current;
+current += previous;
+previous = temp;
+if (current % 2 == 0) {
+sum += current;
+}
+}
+printf("%d\n", sum);
+return 0;
 }
 
