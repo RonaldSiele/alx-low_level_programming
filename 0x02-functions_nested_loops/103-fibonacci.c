3b7650546cbbef1fa274bdb791ccb91be694d845
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 10:46:15 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int current = 2;
+    int previous = 1;
+    int temp;
+    int sum = 2;
+
+    while (current <= 4000000) {
+        temp = current;
+        current += previous;
+        previous = temp;
+        if (current % 2 == 0) {
+            sum += current;
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
