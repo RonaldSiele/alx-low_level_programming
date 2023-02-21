e67ac7372813cd7ca01fdfd1b1ea3d2cdb2b7823
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 00:39:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch = 'a';  
+printf("Lowercase English Alphabets:\n");  
+while(ch <= 'z')  
+{  
+printf("%c ", ch);  
+ch++;  
+}  
+printf("\n");  
+return (0);
+}
