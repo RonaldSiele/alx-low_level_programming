286b5a46c5d172ba3633b45d20c854ae93aa4d20
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 08:26:14 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - prints alpha 10 times
  */
 void print_alphabet_x10(void)
 {
 char j;
-int i = 0 ;
+int i = 0;
 while (i <= 9)
 {
 for (j = 'a'; j <= 'Z'; j++)
