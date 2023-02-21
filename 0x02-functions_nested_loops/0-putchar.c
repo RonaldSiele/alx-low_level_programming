f9f34d51ae04b8e534c005a01246ff863754d50b
Ronald Siele <kimutaironald98@gmail.com>
2023-02-21 03:25:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include<stdio.h>
+root@d690682aecdb:/tmp/alx-low_level_programming/0x02-functions_nested_loops# cat 0-putchar.c 
+#include "main.h"
+
 /**
  * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+
+        return (0);
 }
