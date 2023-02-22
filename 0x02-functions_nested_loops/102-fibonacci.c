98e1f20a492e2ee6fcda91e116ea62e8e1f11da5
Ronald Siele <kimutaironald98@gmail.com>
2023-02-22 07:26:42 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,18 +3,16 @@
 *
 *main-counts the fibonnaci
 */
-#include <stdio.h>
-
 int main() {
-int a = 1, b = 2, c, i;
-printf("%d, %d, ", a, b);
-for(i=2; i<50; i++) {
-c = a + b;
-printf("%d, ", c);
-a = b;
-b = c;
-}
-printf("\n");
-return 0;
+    int a = 1, b = 2, c, i;
+    printf("%d, %d, ", a, b);
+    for(i=2; i<50; i++) {
+        c = a + b;
+        printf("%d, ", c);
+        a = b;
+        b = c;
+    }
+    printf("\n");
+    return 0;
 }
 
