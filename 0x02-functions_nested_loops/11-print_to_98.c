6f93ff911710a3bddded634e21ca48b9535a9574
Ronald Siele <kimutaironald98@gmail.com>
2023-02-22 06:22:30 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,23 +4,27 @@
  *@n: input
  *
  */
-void print_to_98(int n) {
-    int i;
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+void print_to_98(int n)
+{
+int i;
+if (n <= 98) {
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98) 
+{
+printf(", ");
+}
+}
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
 }
 
