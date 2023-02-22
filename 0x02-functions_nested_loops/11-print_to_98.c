8d585a316b77a66a44706c53c1120be221dedb3a
Ronald Siele <kimutaironald98@gmail.com>
2023-02-22 06:40:53 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,21 +4,21 @@
 *@n: input
 *
 */
+#include <stdio.h>
+
 void print_to_98(int n) {
 int i;
 if (n <= 98) {
-for (i = n; i <= 98; i++)
-{
+for (i = n; i <= 98; i++) {
 printf("%d", i);
-if (i != 98){
+if (i != 98) {
 printf(", ");
 }
 }
 } else {
-for (i = n; i >= 98; i--)
-{
+for (i = n; i >= 98; i--) {
 printf("%d", i);
-if (i != 98){
+if (i != 98) {
 printf(", ");
 }
 }
