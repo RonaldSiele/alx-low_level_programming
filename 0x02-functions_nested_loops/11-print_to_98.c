123bf08cd7113bf83dd71a0eaf6ac2412a7a66cc
Ronald Siele <kimutaironald98@gmail.com>
2023-02-22 06:36:32 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 /**
- *print_to_98-prints numbers separated by a comma
- *@n: input
- *
- */
+*print_to_98-prints numbers separated by a comma
+*@n: input
+*
+*/
 void print_to_98(int n) {
-    int i;
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i != 98) {
-                printf(", ");
-            }
-        }
-    }
-    printf("\n");
+int i;
+if (n <= 98) {
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98){
+printf(", ");
+}
+}
+} else {
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98){
+printf(", ");
+}
+}
+}
+printf("\n");
 }
 
